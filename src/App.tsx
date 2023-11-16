07f1a57ea6fc735510b4443bd3946c11254df054
Nante <nantemino15@gmail.com>
2023-11-16 22:54:07 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import SidebarComponent from "./components/sidebar/components/sidebar-component/sidebar.component";
+
+interface AppProps {
+  children: JSX.Element;
+}
+
+function App({ children }: AppProps) {
+  return (
+    <>
+      <SidebarComponent>{children}</SidebarComponent>
+    </>
+  );
+}
+
+export default App;
+
